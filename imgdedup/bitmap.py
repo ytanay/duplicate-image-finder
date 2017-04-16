101b22db30e9bc5e7682b21bf892d207c18365a3
Yotam Tanay <yotam@paticon.com>
2017-04-16 19:08:55 +0300

    Rotations for bitmaps

--- imgdedup/bitmap.py
+++ imgdedup/bitmap.py
@@ -24,6 +24,17 @@ class Bitmap(object):
             output.append([self.bitmap[original_y][int(x // ratio)] for x in range(width)])
 
         return Bitmap(output)
+
+    def rotate(self, angle):
+        angle = math.radians(-angle)
+        output = [[color.WHITE] * self.width for _ in range(self.height)]
+        s, c, hw, hh = math.sin(angle), math.cos(angle), self.width//2, self.height//2
+        for y in range(self.height):
+            for x in range(self.width):
+                x1 = x - hw
+                y1 = y - hh
+                output[y][x] = self.get(int(x1 * c + y1 * s + hw), int(-x1 * s + y1 * c + hh), color.WHITE)
+        return Bitmap(output)
     def get(self, x, y, default=None):
         if 0 <= x < self.width and 0 <= y < self.height:
             return self.bitmap[y][x]
