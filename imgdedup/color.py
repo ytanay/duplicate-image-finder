98f12f0a1977136cd7da706e7597349a7c14986b
Yotam Tanay <yotam@paticon.com>
2017-04-16 19:03:16 +0300

    More useful Color class

--- imgdedup/color.py
+++ imgdedup/color.py
@@ -1,6 +1,16 @@
 from collections import namedtuple
 
 Color = namedtuple('Color', ['red', 'green', 'blue'])
+class Color(object):
+
+    def __init__(self, red, green, blue):
+        self.red, self.green, self.blue = red, green, blue
+
+    def __str__(self):
+        return 'Color(red={}, green={}, blue={})'.format(self.red, self.green, self.blue)
+
+    def __iter__(self):
+        return iter((self.red, self.green, self.blue))
 
 
 RED = Color(red=255, green=0, blue=0)
