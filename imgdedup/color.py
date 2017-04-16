61b56fdcfe708aecc704a125c0ea3323bf817feb
Yotam Tanay <yotam@paticon.com>
2017-04-16 19:05:02 +0300

    Greyscale, difference and average methods for colors

--- imgdedup/color.py
+++ imgdedup/color.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 
-Color = namedtuple('Color', ['red', 'green', 'blue'])
 class Color(object):
 
     def __init__(self, red, green, blue):
@@ -14,6 +13,17 @@ class Color(object):
 
     def hex(self):
         return "#%02x%02x%02x" % (self.red, self.green, self.blue)
+
+    def greyscale(self):
+        return Color(*(sum(self) // 3,) * 3)
+
+    def difference(self, other):
+        return Color(*(abs(x - y) for x, y in zip(self, other)))
+
+    @classmethod
+    def average(cls, *args):
+        return cls(*(sum(vals) // len(args) for vals in zip(*args)))
+
 
 
 RED = Color(red=255, green=0, blue=0)
